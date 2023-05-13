5518214fe569c1513a5eec87dd8d236aef917416
kunjinkaottt <110094636+kunjinkaottt@users.noreply.github.com>
2023-05-13 17:45:47 +0800

    vue.config.js添加 publicPath:'./'

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath:'./',  //解决打包的dist文件夹中的index.html打开为空白界面 因为默认 / 引用不到js文件
   transpileDependencies: true,
   lintOnSave:false
 })
